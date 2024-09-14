c124168e913cd05025736ed90045bd4faa7c5cc2
David Lowry-Duda <david@lowryduda.com>
2024-09-14 14:07:51 -0400

    Query
    We add a simple querying layer. We can now have the following session.
    
    Searching should be added somewhere, but perhaps this isn't the place
    for it. I think I'll include search through a plugin system.
    
    ```
    from dbbase.models import Model, IntegerField, TextField
    from dbbase.query import QueryableModel
    from dbbase.core import DBBase
    
    class Post(Model, QueryableModel):
        table_name = "posts"
        id = IntegerField(primary_key=True)
        title = TextField(nullable=False)
        content = TextField(nullable=False)
    
    db = DBBase('my_database.db')
    db.connect()
    
    Post.create_table(db)
    
    Post.create(db, title="First Post", content="This is the first post")
    Post.create(db, title="Second Post", content="This is the second post")
    Post.create(db, title="Third Post", content="This is the third post")
    
    posts = Post.objects(db).filter(title="Second Post").all()
    for post in posts:
        print(post['title'], post['content'])
    
    post = Post.objects(db).get(id=1)
    if post:
        print(f"Post 1: {post['title']} - {post['content']}")
    
    ordered_posts = Post.objects(db).order_by('title').all()
    for post in ordered_posts:
        print(post['title'], post['content'])
    
    db.close()
    ```

--- dbbase/query.py
+++ dbbase/query.py
@@ -21,3 +21,89 @@ query.py - CRUD and custom SQL queries
 #                 <http://www.gnu.org/licenses/>.
 # **********************************************************************
 """
+from dbbase.core import DBBase, DatabaseConnectionError
+
+
+class QueryBuilder:
+    """
+    A utility class to help build SQL queries programmatically.
+    """
+    def __init__(self, table_name):
+        self.table_name = table_name
+        self.filters = []
+        self.ordering = None
+
+    def add_filter(self, **kwargs):
+        """Add filters to the query."""
+        for key, value in kwargs.items():
+            self.filters.append((key, value))
+
+    def set_order(self, field_name, descending=False):
+        """Set ordering for the query."""
+        direction = "DESC" if descending else "ASC"
+        self.ordering = f"{field_name} {direction}"
+
+    def build_select(self):
+        """Build the SQL SELECT query based on filters and ordering."""
+        query = f"SELECT * FROM {self.table_name}"
+        if self.filters:
+            filter_clauses = " AND ".join([f"{key} = ?" for key, _ in self.filters])
+            query += f" WHERE {filter_clauses}"
+        if self.ordering:
+            query += f" ORDER BY {self.ordering}"
+        return query, tuple(value for _, value in self.filters)
+
+
+class QuerySet:
+    """
+    A class representing a collection of model instances that can be filtered,
+    ordered, and retrieved from the database.
+    """
+    def __init__(self, model, db: DBBase):
+        self.model = model
+        self.db = db
+        self.query_builder = QueryBuilder(model.table_name)
+
+    def filter(self, **kwargs):
+        """
+        Add filtering conditions to the query.
+        Example:
+            Post.filter(title="My Title")
+        """
+        self.query_builder.add_filter(**kwargs)
+        return self
+
+    def order_by(self, field_name, descending=False):
+        """
+        Set ordering for the query.
+        Example:
+            Post.order_by('title')
+        """
+        self.query_builder.set_order(field_name, descending)
+        return self
+
+    def all(self):
+        """Fetch all records matching the query."""
+        query, params = self.query_builder.build_select()
+        return self.db.fetch_all(query, params)
+
+    def get(self, **kwargs):
+        """
+        Fetch a single record matching the filter conditions.
+        Example:
+            Post.get(id=1)
+        """
+        self.filter(**kwargs)
+        query, params = self.query_builder.build_select()
+        return self.db.fetch_one(query, params)
+
+
+# Extend the Model class to integrate querying functionality
+class QueryableModel:
+    """
+    A mixin class that adds querying capabilities to models.
+    """
+    @classmethod
+    def objects(cls, db: DBBase):
+        """Return a QuerySet for the model."""
+        return QuerySet(cls, db)
